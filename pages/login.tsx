459d8b2c908c9c00a724a1b1236888c79b08ce75
LeeCherry731 <sangevol0@gmail.com>
2021-09-11 11:18:54 +0700

    add package about redux

---
+++ pages/login.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+    
+}
+
+export default function login({}: Props): ReactElement {
+    return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    )
+}
